85baa4d16e506563c8598d956b3f079ba8838dba
Marion <contact@marioncasters.fr>
2023-10-15 15:09:45 +0200

    Try to fix image bug : remove hostname and port from server -bis

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || '3000';
+const port = parseInt(process.env.PORT, 10) || 3000;
 const hostname = 'localhost';
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -12,7 +12,7 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(port);
+  }).listen();
 
   console.log(
     `> Server listening at http://localhost:${port} as ${
