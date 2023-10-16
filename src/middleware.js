ecaed5bffe71220d7ce2b70879d67d7d7f6e20d9
Marion <contact@marioncasters.fr>
2023-10-16 12:00:23 +0200

    Revert "Try to fix image issue : remove middleware"
    This reverts commit 754c1ac9e7bc7f43868984b1df5fb172a4f5db95.

---
+++ src/middleware.js
@@ -0,0 +1,13 @@
+import { withAuth } from 'next-auth/middleware';
+
+export default withAuth(function middleware(req) {}, {
+  callbacks: {
+    authorized: ({ req, token }) => {
+      if (req.nextUrl.pathname.startsWith('/admin')) {
+        return !!token;
+      }
+    },
+  },
+});
+
+export const config = { matcher: ['/admin', '/admin/:path*'] };
