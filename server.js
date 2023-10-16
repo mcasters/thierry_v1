d66ccba0553ba10196e9887b8b8b6af63af27a65
Marion <contact@marioncasters.fr>
2023-10-16 08:18:22 +0200

    Try to fix image issue : add url and http package

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const hostname = 'localhost';
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev, hostname, port });
-const handle = app.getRequestHandler();
+const port = parseInt(process.env.PORT || '3000', 10)
+const hostname = 'localhost'
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
 
   console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`,
-  );
-});
+      `> Server listening at http://localhost:${port} as ${
+          dev ? 'development' : process.env.NODE_ENV
+      }`
+  )
+})
