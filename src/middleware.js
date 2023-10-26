1be7539c8e71de960632872e24872f85f20c8610
Marion <contact@marioncasters.fr>
2023-10-26 22:07:35 +0200

    change matcher in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,19 @@
-import { withAuth } from 'next-auth/middleware';
+import { withAuth } from "next-auth/middleware";
 
 export default withAuth(function middleware(req) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
-      if (req.nextUrl.pathname.startsWith('/admin')) {
+      if (req.nextUrl.pathname.startsWith("/admin")) {
         return !!token;
       }
     },
   },
 });
 
-export const config = { matcher: ['/admin', '/admin/:path*'] };
+export const config = {
+  matcher: [
+    "/admin",
+    "/admin/:path*",
+    "/((?!api|_next/static|_next/image|images|favicon.ico).*)",
+  ],
+};
