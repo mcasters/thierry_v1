b948db2e17cea0fd184ba78f6260646a8fcb5011
Marion <contact@marioncasters.fr>
2023-10-29 17:22:50 +0100

    Try to fix image issue

--- src/components/image/ImageLightbox.tsx
+++ src/components/image/ImageLightbox.tsx
@@ -1,110 +1,110 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import Image from 'next/image'
-import Lightbox from 'yet-another-react-lightbox'
-import 'yet-another-react-lightbox/styles.css'
-import { TYPE } from '@/constants'
+import { useState } from "react";
+import Image from "next/image";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { TYPE } from "@/constants";
 
-import s from './lightbox.module.css'
-import { Image as IImage, PostImage } from '.prisma/client'
-import { getSrcItem } from '@/utils/commonUtils'
+import s from "./lightbox.module.css";
+import { Image as IImage, PostImage } from ".prisma/client";
+import { getSrcItem } from "@/utils/commonUtils";
 
 type Props = {
-    images: IImage[] | PostImage[]
-    type: string
-    alt: string
-    heightVh?: number
-    isCentred?: boolean
-}
+  images: IImage[] | PostImage[];
+  type: string;
+  alt: string;
+  heightVh?: number;
+  isCentred?: boolean;
+};
 
-const deviceSizes = [3840, 2048, 1920, 1200, 1080, 750, 640]
+const deviceSizes = [3840, 2048, 1920, 1200, 1080, 750, 640];
 
 const nextImageUrl = (src: string, size: number) =>
-    `/_next/image?url=${encodeURIComponent(src)}&w=${size}&q=75`
+  `/_next/image?url=${encodeURIComponent(src)}&w=${size}&q=75`;
 
 export default function ImageLightbox({
-    images,
-    type,
-    alt,
-    heightVh,
-    isCentred,
+  images,
+  type,
+  alt,
+  heightVh,
+  isCentred,
 }: Props) {
-    const oneImage = type === TYPE.PAINTING || images.length === 1
-    const [open, setOpen] = useState(false)
-    const slides = images.map((image) => ({
-        width: image.width,
-        height: image.height,
-        src: nextImageUrl(`${getSrcItem(type, image.filename)}`, image.width),
-        srcSet: deviceSizes
-            .filter((size) => size <= image.width)
-            .map((size) => ({
-                src: nextImageUrl(`${getSrcItem(type, image.filename)}`, size),
-                width: size,
-                height: Math.round((image.height / image.width) * size),
-            })),
-    }))
+  const oneImage = type === TYPE.PAINTING || images.length === 1;
+  const [open, setOpen] = useState(false);
+  const slides = images.map((image) => ({
+    width: image.width,
+    height: image.height,
+    src: nextImageUrl(`${getSrcItem(type, image.filename)}`, image.width),
+    srcSet: deviceSizes
+      .filter((size) => size <= image.width)
+      .map((size) => ({
+        src: nextImageUrl(`${getSrcItem(type, image.filename)}`, size),
+        width: size,
+        height: Math.round((image.height / image.width) * size),
+      })),
+  }));
 
-    return (
-        <div
-            className={
-                !oneImage
-                    ? s.imageGridContainer
-                    : isCentred
-                    ? s.imageCentredContainer
-                    : undefined
-            }
-        >
-            {images.map((image) => {
-                return (
-                    <button
-                        key={image.id}
-                        type="button"
-                        onClick={() => setOpen(true)}
-                        className={s.imageWrap}
-                        style={{
-                            height: heightVh ? `${heightVh}vh` : '45vh',
-                            width: '100%',
-                        }}
-                    >
-                        <Image
-                            src={`/images/${type}/${image.filename}`}
-                            fill={true}
-                            style={{
-                                objectFit: 'contain',
-                            }}
-                            alt={alt}
-                        />
-                    </button>
-                )
-            })}
+  return (
+    <div
+      className={
+        !oneImage
+          ? s.imageGridContainer
+          : isCentred
+          ? s.imageCentredContainer
+          : undefined
+      }
+    >
+      {images.map((image) => {
+        return (
+          <button
+            key={image.id}
+            type="button"
+            onClick={() => setOpen(true)}
+            className={s.imageWrap}
+            style={{
+              height: heightVh ? `${heightVh}vh` : "45vh",
+              width: "100%",
+            }}
+          >
+            <Image
+              src={`/images/${type}/${image.filename}`}
+              fill={true}
+              style={{
+                objectFit: "contain",
+              }}
+              alt={alt}
+            />
+          </button>
+        );
+      })}
 
-            {oneImage && (
-                <Lightbox
-                    open={open}
-                    close={() => setOpen(false)}
-                    slides={slides}
-                    controller={{
-                        closeOnPullDown: true,
-                        closeOnBackdropClick: true,
-                    }}
-                    render={{
-                        buttonPrev: () => null,
-                        buttonNext: () => null,
-                    }}
-                />
-            )}
-            {!oneImage && (
-                <Lightbox
-                    open={open}
-                    close={() => setOpen(false)}
-                    slides={slides}
-                    controller={{
-                        closeOnPullDown: true,
-                        closeOnBackdropClick: true,
-                    }}
-                />
-            )}
-        </div>
-    )
+      {oneImage && (
+        <Lightbox
+          open={open}
+          close={() => setOpen(false)}
+          slides={slides}
+          controller={{
+            closeOnPullDown: true,
+            closeOnBackdropClick: true,
+          }}
+          render={{
+            buttonPrev: () => null,
+            buttonNext: () => null,
+          }}
+        />
+      )}
+      {!oneImage && (
+        <Lightbox
+          open={open}
+          close={() => setOpen(false)}
+          slides={slides}
+          controller={{
+            closeOnPullDown: true,
+            closeOnBackdropClick: true,
+          }}
+        />
+      )}
+    </div>
+  );
 }
