1099ff69b805d193a651a97a1e4dc9b0488af90e
Marion <contact@marioncasters.fr>
2023-11-17 15:51:51 +0100

    Try to fix image issue : latest version of next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    formats: ["image/webp"],
-    minimumCacheTTL: 60,
-  },
-};
+const nextConfig = {};
 module.exports = nextConfig;
