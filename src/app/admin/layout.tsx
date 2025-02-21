0d5d28d73b7d77f16fee7c3f7cf246c703dff284
Marion <contact@marioncasters.fr>
2025-02-22 00:32:22 +0100

    Session check on admin Layout page

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-
 import AdminLayout from "@/components/layout/admin/AdminLayout";
 import { Metadata } from "next";
 import { DESCRIPTION, DOCUMENT_TITLE } from "@/constants/specific/metaHtml";
+import { getSession } from "@/app/lib/auth";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: DOCUMENT_TITLE.ADMIN,
   description: DESCRIPTION.ADMIN,
 };
 
-export default function layout({ children }: { children: React.ReactNode }) {
-  return <AdminLayout>{children}</AdminLayout>;
+export default async function layout({ children }: { children: ReactNode }) {
+  const session = await getSession();
+
+  if (session) {
+    return <AdminLayout>{children}</AdminLayout>;
+  } else {
+    redirect("/login");
+  }
 }
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,5 @@
 import AdminTheme from "@/components/admin/theme/AdminTheme";
 import { AdminWorkThemeProvider } from "@/app/context/adminWorkThemeProvider";
-import { redirect } from "next/navigation";
-import { getSession } from "@/app/lib/auth";
 import {
   getActiveTheme,
   getPresetColors,
@@ -9,19 +7,14 @@ import {
 } from "@/app/actions/theme";
 
 export default async function Page() {
-  const session = await getSession();
   const themes = await getThemesFull();
   const presetColors = await getPresetColors();
   let activeTheme = themes.filter((t) => t.isActive)[0];
   if (!activeTheme) activeTheme = await getActiveTheme();
 
-  if (session) {
-    return (
-      <AdminWorkThemeProvider defaultWorkTheme={activeTheme}>
-        <AdminTheme themes={themes} presetColors={presetColors} />
-      </AdminWorkThemeProvider>
-    );
-  } else {
-    redirect("/home");
-  }
+  return (
+    <AdminWorkThemeProvider defaultWorkTheme={activeTheme}>
+      <AdminTheme themes={themes} presetColors={presetColors} />
+    </AdminWorkThemeProvider>
+  );
 }
