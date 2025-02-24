9852d9b3db8a12e68e4c7d6b1223ab5b78c0e1a6
Marion <contact@marioncasters.fr>
2025-02-24 19:39:01 +0100

    Custom server with next handler

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-const { createServer } = require("http");
 const express = require("express");
-const { parse } = require("url");
 const next = require("next");
 
-const port = parseInt(process.env.PORT || "3000", 10);
-const hostname = "localhost" || "127.0.0.1";
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const expressApp = express();
+  const server = express();
 
-  expressApp.use("/images", express.static(process.env.PHOTOS_PATH));
+  server.use("/images", express.static(process.env.PHOTOS_PATH));
 
-  expressApp.all("*", (req, res) => {
+  server.all("*", (req, res) => {
     return handle(req, res);
   });
 
-  createServer(expressApp).listen(port, (err) => {
+  server.listen(3000, (err) => {
     if (err) throw err;
     console.log(
-      `> Ready on http://${hostname}:${port} as ${
+      `> Ready on http://localhost:3000 as ${
         dev ? "development" : process.env.NODE_ENV
       }`,
     );
