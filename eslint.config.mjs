76ed04f3106d1a7cfc96deb55b99938c902a73cd
Marion <contact@marioncasters.fr>
2025-02-28 19:23:03 +0100

    Configuration and update some modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
 import {FlatCompat} from '@eslint/eslintrc';
 import eslintConfigPrettier from "eslint-config-prettier";
+import {fileURLToPath} from "url";
+import {dirname} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-})
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = [
   ...compat.config({
     extends: ['next', 'next/core-web-vitals', 'next/typescript'],
