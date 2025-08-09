6683b9aeba1ddbf1f3df47e5eb90fed6fd500b6d
Marion <contact@marioncasters.fr>
2025-08-09 10:00:04 +0200

    Improve dragging feature for modal

--- src/components/admin/form/modal.tsx
+++ src/components/admin/form/modal.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import React, { ReactNode, useEffect } from "react";
+import React, { ReactNode, useEffect, useRef, useState } from "react";
 
 import s from "../modal.module.css";
-import useOnMousePosition from "@/components/hooks/useOnMousePosition.ts";
 
 interface Props {
   children: ReactNode;
@@ -20,12 +19,31 @@ export default function Modal({
   onClickOutside,
   width = 730,
 }: Props) {
-  const { location, ref } = useOnMousePosition();
+  const ref = useRef<HTMLDivElement>(null!);
+  const [location, setLocation] = useState<{
+    x: number;
+    y: number;
+  }>({ x: 0, y: 0 });
+  const [styles, setStyles] = useState({});
 
   useEffect(() => {
     document.body.style.overflow = isOpen ? "hidden" : "unset";
     document.body.style.userSelect = isOpen ? "none" : "unset";
   }, [isOpen]);
+
+  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    if (e.currentTarget) {
+      const boundingRect = e.currentTarget.getBoundingClientRect();
+      setLocation({
+        x: e.screenX - boundingRect.left,
+        y: e.screenY - boundingRect.top,
+      });
+    }
+  };
+
+  const onDrag = (e: React.DragEvent<HTMLDivElement>) => {
+    setStyles({ left: e.screenX - location.x, top: e.screenY - location.y });
+  };
 
   return (
     <>
@@ -33,26 +51,23 @@ export default function Modal({
         <div
           className={s.modalOverlay}
           onClick={() => (onClickOutside ? onClickOutside() : undefined)}
-          // set "draggable" to be able to remove draggable with "onDragStart" (inherit from dragList)
-          draggable
-          onDragStart={(e) => {
-            e.preventDefault();
-          }}
         >
           <div
             ref={ref}
             className={s.modalBox}
-            style={
-              location
-                ? { width, top: location.y, left: location.x }
-                : { width }
-            }
+            style={{ ...styles, width }}
             onClick={(e) => {
               e.stopPropagation();
             }}
+            draggable
+            onDragStart={onDragStart}
+            onDrag={onDrag}
+            onDragOver={(e) => e.preventDefault()}
           >
             <h2 className={s.modalTitle}>{title}</h2>
-            {children}
+            <div draggable onDragStart={(e) => e.preventDefault()}>
+              {children}
+            </div>
           </div>
         </div>
       )}
