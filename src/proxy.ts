956a7810e44e6ce1734121712f1da740e8d5b29a
Marion <contact@marioncasters.fr>
2026-02-13 23:36:26 +0100

    Next : Changing middleware file to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { COOKIE_NAME } from "@/constants/admin";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const currentUser = request.cookies.get(COOKIE_NAME)?.value;
 
   if (!currentUser && request.nextUrl.pathname.startsWith("/admin")) {
