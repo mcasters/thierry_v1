650eeb1946ac243d7627049af866822d0dbeb920
Marion <contact@marioncasters.fr>
2026-03-06 12:41:09 +0100

    Upgrade to V2 : server

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
-import express from "express";
+import { createServer } from "http";
 import next from "next";
 
+const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
+  createServer((req, res) => {
+    handle(req, res);
+  }).listen(port);
 
-  server.use("/images", express.static(process.env.PHOTOS_PATH));
-  server.use((req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(3000, (err) => {
-    if (err) throw err;
-    console.log(
-      `> Ready on http://localhost:3000 as ${
-        dev ? "development" : process.env.NODE_ENV
-      }`,
-    );
-  });
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? "development" : process.env.NODE_ENV
+    }`,
+  );
 });
